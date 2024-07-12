a3d5e7b5e8600f61a4fb80b71f2efa4d4b8b09d1
Daruin99 <102298637+Daruin99@users.noreply.github.com>
2024-07-12 19:50:46 +0300

    Update JWT Filter

--- e-commerce/src/main/java/com/finalProject/e_commerce/filter/JwtFilter.java
+++ e-commerce/src/main/java/com/finalProject/e_commerce/filter/JwtFilter.java
@@ -64,7 +64,6 @@ public class JwtFilter extends OncePerRequestFilter {
         UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
                 userDetails, null, userDetails.getAuthorities()
         );
-        authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
         SecurityContextHolder.getContext().setAuthentication(authentication);
         chain.doFilter(request, response);
     }
